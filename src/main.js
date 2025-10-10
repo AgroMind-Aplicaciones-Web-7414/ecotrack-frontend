b99fe29f9ce7f24f835b1426992cd5c40c14021b
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-10 18:36:58 -0500

    feat(fake-api): add json-server and configure fake api.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import { router } from './router.js';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import router from './router.js'
+import PrimeVue from 'primevue/config'
+import Material from '@primeuix/themes/material'
+import {Button, FloatLabel, Card, Checkbox, InputText, DatePicker, Toolbar} from 'primevue'
+import 'primeicons/primeicons.css'
 
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
 
-const app = createApp(App);
-
-// Loguea cualquier error de runtime en consola
-app.config.errorHandler = (err, instance, info) => {
-    console.error('[VueError]', err, info);
-};
-
-app.use(router);
-app.use(PrimeVue, { theme: { preset: Aura } });
-app.mount('#app');
+createApp(App)
+    .use(PrimeVue,{theme: {preset: Material}, ripple: true})
+    .component('pv-toolbar', Toolbar)
+    .component('pv-button', Button)
+    .component('pv-floatlabel', FloatLabel)
+    .component('pv-card', Card)
+    .component('pv-checkbox', Checkbox)
+    .component('pv-datepicker', DatePicker)
+    .component('pv-input-text', InputText)
+    .use(router)
+    .mount('#app')
