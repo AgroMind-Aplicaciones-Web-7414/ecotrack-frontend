7cc21aa890a8b475b43952665955653d9f9cefc0
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-11 05:17:02 -0500

    feat: add production environment variable to configuration

--- src/cultivation/domain/model/cultivation.entity.js
+++
@@ -1,9 +0,0 @@
-export class Crop{
-    constructor({id = null, name, surface, location, plantingDate}){
-        this.id = id
-        this.name = name
-        this.surface = surface
-        this.location = location
-        this.plantingDate = plantingDate
-    }
-}
